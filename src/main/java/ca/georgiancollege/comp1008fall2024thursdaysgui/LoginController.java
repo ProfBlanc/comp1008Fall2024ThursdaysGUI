402f15c05bf6e730ee06ee747e9d013e61b8885f
Ben <ben.blanc@georgebrown.ca>
2024-11-28 14:27:58 -0500

    Nov 28 Class Code @ 14h30

--- src/main/java/ca/georgiancollege/comp1008fall2024thursdaysgui/LoginController.java
+++ src/main/java/ca/georgiancollege/comp1008fall2024thursdaysgui/LoginController.java
@@ -1,4 +1,65 @@
 package ca.georgiancollege.comp1008fall2024thursdaysgui;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+
 public class LoginController {
+
+
+    @FXML
+    private TextField txtEmail;
+
+    @FXML
+    private PasswordField txtPassword;
+
+    private void example1(){
+
+        System.out.println("Login Clicked");
+
+        System.out.println(txtEmail.getText());
+        System.out.println(txtPassword.getText());
+
+
+        String email = txtEmail.getText();
+        String password = txtPassword.getText();
+
+        if(
+                email.equals("admin") && password.equals("password")
+                        ||
+                        email.equals("firstname") && password.equals("lastname")
+                        ||
+                        email.equals("hello") && password.equals("world")
+        ){
+            System.out.println("Correct credentials ");
+        }
+        else{
+            System.out.println("Incorrect credentials ");
+        }
+
+
+    }
+
+    private LoginModel model = new LoginModel();
+
+    @FXML
+    void onLoginClick(ActionEvent event){
+
+    }
+
+    @FXML
+    void onRegisterClick(ActionEvent event){
+        //System.out.println("Register Clicked");
+        try {
+            model.registerUser(txtEmail.getText(), txtPassword.getText());
+        }
+        catch (Exception e){
+            Alert alert = new Alert(Alert.AlertType.ERROR);
+            alert.setTitle("Registration Error");
+            alert.setContentText(e.getMessage());
+            alert.show();
+        }
+    }
 }
--- src/main/java/ca/georgiancollege/comp1008fall2024thursdaysgui/LoginModel.java
+++ src/main/java/ca/georgiancollege/comp1008fall2024thursdaysgui/LoginModel.java
@@ -1,4 +1,32 @@
 package ca.georgiancollege.comp1008fall2024thursdaysgui;
 
 public class LoginModel {
+
+    /*
+            Create a Constructor
+                determine if a directory with the name of "data" exists in our resources folder
+                    if NOT, we will create it
+                        Path.of(path/to/resources/directory)
+                        Files.Exists or pathObject.toFile().exists()
+
+                        Files.CreateDirectory(path object)
+
+
+            Create method registerUser(String username, String password)
+
+                create a file named username.txt where username is the text content of username
+                    example: admin.txt, ben.txt
+                the file content of this text file is the password of the user
+
+                Bonus: determine if the filename already exists
+                        if so, throw an exception
+
+     */
+
+    public LoginModel(){}
+    public void registerUser(String username, String password){}
+
+
+
+
 }
